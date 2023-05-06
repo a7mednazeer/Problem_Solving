cf56d363fd03414f6b51895393a0095e89089d68
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-05-06 13:10:58 +0200

    4A. 282A. Bit++

---
+++ 282A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    
+    int x = 0;
+     while (t--) {
+        string str;
+	cin >> str;
+        ((str == "x++" || str == "++x") ? x++ : x--);
+     }
+     cout << x << '\n';
+}
