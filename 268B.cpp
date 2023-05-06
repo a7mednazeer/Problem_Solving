72f345b866e22ea6995b795fb43ab666098288d0
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-05-06 13:13:25 +0200

    268B. Buttons

---
+++ 268B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int res = n;
+    for(int i = 1; i < n; i++) {
+        res += ( n - i ) * i;
+    }
+    cout << res << '\n';
+}
