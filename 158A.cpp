414d15fb01748a84d97300d35603996bac70d329
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-07-13 23:28:06 +0300

    158A Next Round

---
+++ 158A.cpp
@@ -0,0 +1,22 @@
+// Next Round
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	short n, k, count;
+	cin>> n >> k;
+
+	short * arr = new short[n];
+
+	for (short i = 0; i < n; i++) {
+		cin>> arr[i];
+	}
+	
+	for (short i = 0; i < n; i++) {
+		if(arr[i] >= arr[k-1] && arr[i] > 0) {
+			count++;
+	    }
+    }
+	cout<< count << '\n';
+	delete [] arr;
+}
