8847f512491f0b0ccee64a08399e921dd12bdaf4
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-07-14 00:05:09 +0300

    1353B. Two Arrays And Swaps

--- 1353B.cpp
+++ 1353B.cpp
@@ -3,33 +3,34 @@ using namespace std;
 
 int main() {
     short t, n, k, sum;
-	cin>> t;
+    cin>> t;
 
-	while (t--) {
-	    cin>> n >> k;
+    while (t--) {
+	cin>> n >> k;
         sum = 0;
-	    short * a = new short[n];
-	    for (short i = 0; i < n; i++) {
-	        cin>> a[i];
-	    }
-    
-	    short * b = new short[n];
-	    for (short i = 0; i < n; i++) {
-	        cin>> b[i];
-	    }
-
-		sort(a , a+n);
-		sort(b , b+n);
-
-		for (short i = 0; i < k; i++) {
-			if (a[i] <= b[n-i-1]) {
-		        swap(a[i] , b[n-i-1]);
-			}
-	    }	
-
-		for (short i = 0; i < n; i++) {
-	         sum += a[i];
-	    }
-		cout<< sum <<'\n';
+	
+	short * a = new short[n];
+	for (short i = 0; i < n; i++) {
+	    cin>> a[i];
 	}
+    
+	short * b = new short[n];
+        for (short i = 0; i < n; i++) {
+	    cin>> b[i];
+        }
+
+	sort(a , a+n);
+  	sort(b , b+n);
+
+	for (short i = 0; i < k; i++) {
+  	    if (a[i] <= b[n-i-1]) {
+	        swap(a[i] , b[n-i-1]);
+	    }
+	}	
+
+	for (short i = 0; i < n; i++) {
+	    sum += a[i];
+	}
+	cout<< sum <<'\n';
+    }
 }
